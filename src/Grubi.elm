7bc36ed3ffb02aeee6d358e27fae8709bb49943b
Mini0n <mini0n.000@gmail.com>
2019-02-13 12:11:14 -0600

    updated Grubi with state and generated elm.js

--- src/Grubi.elm
+++ src/Grubi.elm
@@ -6,7 +6,7 @@ import Html.Attributes exposing (..)
 
 view model =
     div [ class "content" ]
-        [ h1 [] [ pre [] [ text "Grubi" ] ]
+        [ h1 [] [ text "Grubi" ]
         , div [ id "thumbs" ]
             [ img [ src "https://media.giphy.com/media/xWBjMpOr7rMJ00XA3Y/200w_d.gif" ] []
             , img [ src "https://media.giphy.com/media/2t9sbaLKTaDWeSFhqr/200w_d.gif" ] []
@@ -15,5 +15,12 @@ view model =
         ]
 
 
+initialModel =
+    [ { url = "1.gif" }
+    , { url = "2.gif" }
+    , { url = "3.gif" }
+    ]
+
+
 main =
-    view "no model yet"
+    view initialModel
