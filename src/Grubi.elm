97e05136c68239bff779f63125ace7785a8d598d
Mini0n <mini0n.000@gmail.com>
2019-02-13 17:29:12 -0600

    Added aliases and annotations

--- src/Grubi.elm
+++ src/Grubi.elm
@@ -1,23 +1,25 @@
 module Grubi exposing (main)
 
+import Array exposing (Array)
+import Browser
 import Html exposing (div, h1, h4, img, pre, text)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 
 
-
--- original static view
--- view model =
---     div [ class "content" ]
---         [ h1 [] [ text "Grubi" ]
---         , div [ id "thumbs" ]
---             [ img [ src "https://media.giphy.com/media/xWBjMpOr7rMJ00XA3Y/200w_d.gif" ] []
---             , img [ src "https://media.giphy.com/media/2t9sbaLKTaDWeSFhqr/200w_d.gif" ] []
---             , img [ src "https://media.giphy.com/media/lzoFgUxKNpR67fAu1l/200w_d.gif" ] []
---             ]
---         ]
+type alias Thumb =
+    { url : String }
 
 
+type alias Click =
+    { data : String, description : String }
+
+
+type alias Model =
+    { thumbs : List Thumb, selected : String }
+
+
+view : Model -> Html.Html Click
 view model =
     div [ class "content" ]
         [ h1 [] [ text "g r u b i" ]
@@ -26,6 +28,7 @@ view model =
         ]
 
 
+initialModel : Model
 initialModel =
     { thumbs =
         [ { url = "xWBjMpOr7rMJ00XA3Y" } -- uh, uhh ♫
@@ -36,25 +39,46 @@ initialModel =
     }
 
 
+thumbArray : Array Thumb
+thumbArray =
+    Array.fromList initialModel.thumbs
+
+
 main =
-    view initialModel
+    Browser.sandbox
+        { init = initialModel
+        , view = view
+        , update = update
+        }
+
+
+update msg model =
+    if msg.description == "ClickedThumb" then
+        { model | selected = msg.data }
+
+    else
+        model
 
 
 
 -- Helper functions
 
 
+viewThumbs : String -> Thumb -> Html.Html Click
 viewThumbs selected thumb =
     img
         [ src (urlThumb thumb.url)
         , classList [ ( "selected", selected == thumb.url ), ( "thumb", True ) ]
+        , onClick { description = "ClickedThumb", data = thumb.url }
         ]
         []
 
 
+urlThumb : String -> String
 urlThumb gifCode =
     "https://media.giphy.com/media/" ++ gifCode ++ "/200w_d.gif"
 
 
+urlLarge : String -> String
 urlLarge gifCode =
     "https://media.giphy.com/media/" ++ gifCode ++ "/giphy.gif"
